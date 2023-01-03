8438e606c387938d835a65c93c443eeb2ade8722
samoldham <106932222+samoldham@users.noreply.github.com>
2023-01-03 17:13:46 +0000

    1 file modified

--- src/app/data.ts
+++ src/app/data.ts
@@ -3,47 +3,53 @@ import { Edge, Node, ClusterNode } from '@swimlane/ngx-graph';
 export const nodes: Node[] = [
   {
     id: 'first',
-    label: 'A'
-  }, {
+    label: 'A',
+  },
+  {
     id: 'second',
-    label: 'B'
-  }, {
+    label: 'B',
+  },
+  {
     id: 'c1',
-    label: 'C1'
-  }, {
+    label: 'C1',
+  },
+  {
     id: 'c2',
-    label: 'C2'
-  }
+    label: 'C2',
+  },
 ];
 
 export const clusters: ClusterNode[] = [
   {
     id: 'third',
     label: 'C',
-    childNodeIds: ['c1', 'c2']
-  }
-]
+    childNodeIds: ['c1', 'c2'],
+  },
+];
 
 export const links: Edge[] = [
   {
     id: 'a',
     source: 'first',
     target: 'second',
-    label: 'is parent of'
-  }, {
+    label: 'is parent of',
+  },
+  {
     id: 'b',
     source: 'first',
     target: 'c1',
-    label: 'custom label'
-  }, {
+    label: 'custom label',
+  },
+  {
     id: 'c',
     source: 'first',
     target: 'c1',
-    label: 'custom label'
-  }, {
+    label: 'custom label',
+  },
+  {
     id: 'd',
     source: 'first',
     target: 'c2',
-    label: 'custom label'
-  }
+    label: 'custom label 1',
+  },
 ];
